828784b65dbbf98e9743f7ba7fee7718e445a134
Kurt Funai <jesuiskurt@gmail.com>
2012-06-06 18:31:17 -0400

    Added changes to the commit collection parse(). It now creates a collection of users, their information, and the number of commits

--- public/js/collections/commit.js
+++ public/js/collections/commit.js
@@ -1,7 +1,8 @@
 define([
     'backbone',
+    'underscore',
     'models/commit'
-], function(Backbone, Commit){
+], function(Backbone, _, Commit){
     return Backbone.Collection.extend({
         model: Commit,
 
@@ -16,6 +17,30 @@ define([
             options = options ? _.clone(options) : {};
             options.url = 'https://api.github.com/repos/' + vendor + '/' + repo + '/commits';
             this.fetch(options);
+        },
+        parse: function(data) {
+            console.log(data);
+            var emails = {};
+            var users = [];
+            _.each(data, function(data){
+                if (!_.has(emails, data.commit.author.email)) {
+                    var user = {};
+                    if (!_.isUndefined(data.author)) {
+                        user.login = data.author.login;
+                        user.github = data.author.url;
+                        user.avatar = data.author.avatar_url;
+                    }
+                    user.email = data.commit.author.email;
+                    user.name = data.commit.author.name;
+                    user.message = data.commit.message;
+                    user.commits = 1;
+                    users.push(user);
+                    emails[data.commit.author.email] = users.length - 1;
+                } else {
+                    users[emails[data.commit.author.email]].commits++;
+                }
+            });
+            return users;
         }
     });
 });
